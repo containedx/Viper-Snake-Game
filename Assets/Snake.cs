900f21eaeb7f1be9d51f21b8bc347b834b04971c
Kinga <niu.ula.a@poczta.fm>
2020-02-29 15:24:39 +0100

    food update, restart game on collision, move faster and other features

--- Assets/Snake.cs
+++ Assets/Snake.cs
@@ -1,23 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq; 
+using System.Linq;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Snake : MonoBehaviour {
 
     //up == default
-    Vector2 direction = Vector2.zero; 
+    Vector2 direction = Vector2.zero;
+
+    bool ate = false;
+
+    public GameObject TailPrefab;
+
+    public Component Score; 
+
+    // track of snake elements
+    List<Transform> tail = new List<Transform>();
+
+    public static int score = 0;
+
+    // to control how fast snake is
+    public float velocity = 0.2f; 
 
 	// Use this for initialization
 	void Start () {
 
-        // moving every 300ms
-        InvokeRepeating("Moving", 0.3f, 0.3f);
-		
-	}
+        // moving every velocity second
+        //InvokeRepeating("Moving", velocity, velocity);   
+
+        StartCoroutine(Faster());
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+        // update score
+        Score.GetComponent<Text>().text = "" + score; 
 
         // on key press change direction 
 
@@ -35,10 +56,68 @@ public class Snake : MonoBehaviour {
 
     }
 
+    IEnumerator Faster()
+    {
+        while(true)
+        {
+            Moving();
+            yield return new WaitForSeconds(velocity);
+        }
+    }
+
     void Moving () {
 
+        // save current position
+        Vector2 headposition = transform.position; 
+
+        // change direction
         transform.Translate(direction);
 
+        if (ate)
+        {
+            // load prefab
+            GameObject tailelement = (GameObject)Instantiate(TailPrefab, headposition, Quaternion.identity);
 
+            // insert at end of snake
+            tail.Insert(0, tailelement.transform);
+
+            // raise score value and velocity
+            score += 1;
+            velocity -= 0.01f; 
+
+            ate = false; 
+        }
+
+        if (tail.Count > 0)  // if==0 => snake has only head
+        {
+            // Move last element to place where head was before changing direction
+            // gap will be filled and movement will be smooth
+            tail.Last().position = headposition;
+
+            // fix sequence of tail
+
+            // Insert first 'tail'
+            tail.Insert(0, tail.Last());
+            // Remove last
+            tail.RemoveAt(tail.Count - 1); 
+        }
+
+
+    }
+
+    void OnTriggerEnter2D( Collider2D coll)
+    {
+        if ( coll.name.StartsWith("ole"))
+        {
+            // eating
+            ate = true;
+            Destroy(coll.gameObject); 
+        }
+        else
+        {
+            // snake dead == restart game
+            SceneManager.LoadScene("level0");
+            score = 0; 
+        }
     }
 }
