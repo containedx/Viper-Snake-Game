550d3c1c1bd9802140d0af150dfa02e5d3fe2900
unknown <containedx.niu.ula.a@gmail.com>
2020-02-29 01:41:43 +0100

    snake moves and food is appearing

---
+++ Assets/Snake.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq; 
+
+public class Snake : MonoBehaviour {
+
+    //up == default
+    Vector2 direction = Vector2.zero; 
+
+	// Use this for initialization
+	void Start () {
+
+        // moving every 300ms
+        InvokeRepeating("Moving", 0.3f, 0.3f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // on key press change direction 
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            direction = Vector2.up;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            direction = Vector2.down;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            direction = Vector2.right;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            direction = Vector2.left;
+
+    }
+
+    void Moving () {
+
+        transform.Translate(direction);
+
+
+    }
+}
---
+++ Assets/SpawnFoodAround.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFoodAround : MonoBehaviour {
+
+    public GameObject FoodPrefab;
+
+    //borders => we cannot cross them
+    public Transform borderUp;
+    public Transform borderDown;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    // Use this for initialization
+    void Start () {
+
+        // Exec Spawn every 4 seconds starting at 3
+        InvokeRepeating("SpawnAround", 3, 4);
+	}
+
+    void SpawnAround () {
+
+        //generate random position (x,y)
+
+        int x, y;
+
+        x = (int)Random.Range(borderLeft.position.x +1, borderRight.position.x -1);
+
+        y = (int)Random.Range(borderDown.position.y +1, borderUp.position.y -1);
+
+        // show       object        vector         rotation (here default)
+        Instantiate(FoodPrefab, new Vector2(x, y), Quaternion.identity); 
+    }
+
+}
